805ac79d820efd19445802144d1efcdd4bdedd78
Jonathan Clem <jonathan@jclem.net>
2016-10-11 18:34:03 -0400

    Add notifications of trackback add/remove

--- sharedb/index.js
+++ sharedb/index.js
@@ -1,17 +1,19 @@
 'use strict';
 
-const cookie = require('cookie');
-const crypto = require('crypto');
 const ShareDB = require('sharedb');
-const shareDBLogger = require('sharedb-logger');
 const ShareDBPostgresCanvas = require('./lib/sharedb-postgres-canvas');
 const ShareDBRedisPubSub = require('sharedb-redis-pubsub');
+const Sidekiq = require('sidekiq');
 const WebSocketJSONStream = require('websocket-json-stream');
 const WebSocketServer = require('ws').Server;
+const { authenticate, getUserID } = require('./lib/authenticate');
 const db = new ShareDBPostgresCanvas();
 const pubsub = new ShareDBRedisPubSub(process.env.REDIS_URL);
+const redis = require('redis').createClient();
+const shareDBLogger = require('sharedb-logger');
+const sidekiq = new Sidekiq(redis, 'exq');
 
-const { SECRET_KEY_BASE, SIGNING_SALT } = process.env;
+const CANVAS_URL = new RegExp(`^${process.env.WEB_URL}/[^/]+/([^/]{22})$`, 'i');
 
 module.exports = function shareDBWsServer(server, options) {
   options = options || {};
@@ -23,33 +25,54 @@ module.exports = function shareDBWsServer(server, options) {
 function onWSConnection(wsConn) {
   const stream = new WebSocketJSONStream(wsConn);
   const shareDB = new ShareDB({ db, pubsub });
-  if (process.env.NODE_ENV !== 'production') shareDBLogger(shareDB);
+  // if (process.env.NODE_ENV !== 'production') shareDBLogger(shareDB);
   shareDB.use('connect', authenticate);
   shareDB.use('receive', pingPong);
+  shareDB.use('after submit', checkTrackbacks);
   shareDB.listen(stream);
 }
 
-function authenticate(req, cb) {
-  crypto.pbkdf2(SECRET_KEY_BASE, SIGNING_SALT, 1000, 32, 'sha256',
-    (err, key) => {
-      const upgradeCookie = req.agent.stream.ws.upgradeReq.headers.cookie;
-      const apiCookie = cookie.parse(upgradeCookie)._canvas_pro_api_key;
-      const [algoName, payload, signature] = apiCookie.split('.');
-      const plainText = algoName + '.' + payload;
-      const challenge = crypto.createHmac('sha256', key);
-      challenge.update(plainText);
+function checkTrackbacks(req, cb) {
+  const newTrackbacks = req.op.op.filter(isTrackback);
+  const oldTrackbacks = req.op.op.filter(isUntrackback);
 
-      if (crypto.timingSafeEqual(challenge.digest(),
-                                 Buffer.from(signature, 'base64'))) {
-        cb();
-      } else {
-        cb(new Error('Invalid session cookie'));
-      }
-    })
+  newTrackbacks.forEach(trackback => {
+    const id = trackback.li.meta.url.match(CANVAS_URL)[1];
+
+    sidekiq.enqueue(
+      'CanvasAPI.CanvasTrackback.Worker',
+      ['add', id, req.op.d, req.agent.stream.ws.accountID], {
+        queue: 'default'
+      });
+  });
+
+  oldTrackbacks.forEach(trackback => {
+    const id = trackback.ld.meta.url.match(CANVAS_URL)[1];
+
+    sidekiq.enqueue(
+      'CanvasAPI.CanvasTrackback.Worker',
+      ['remove', id, req.op.d, req.agent.stream.ws.accountID], {
+        queue: 'default'
+      });
+  });
+
+  cb();
 }
 
-function decode(cookiePart) {
-  return Buffer.from(cookiePart, 'base64').toString();
+function isTrackback(comp) {
+  if (comp.p.length > 1) return false;
+  if (!comp.li) return false;
+  return isCanvasTrackback(comp.li);
+}
+
+function isUntrackback(comp) {
+  if (comp.p.length > 1) return false;
+  if (!comp.ld) return false;
+  return isCanvasTrackback(comp.ld);
+}
+
+function isCanvasTrackback(part) {
+  return part.type === 'url' && CANVAS_URL.test(part.meta.url);
 }
 
 function pingPong(req, cb) {
